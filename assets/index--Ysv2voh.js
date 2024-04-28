7e7559988b668f94121abfdbb202e7034d56ec13
dashuchiu <150155382+dashuchiu@users.noreply.github.com>
2024-04-29 00:23:18 +0800

    v1

---
+++ assets/index--Ysv2voh.js
@@ -0,0 +1,1 @@
+import{b2 as s,F as t,as as o,aY as r,q as n}from"./index-CL_MJS_C.js";const p={__name:"index",setup(c){const e=s(),a=t(e.params.id);return(u,i)=>(n(),o("div",null,r(a.value),1))}};export{p as default};
